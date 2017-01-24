c734c2faa9d25e4f214aab4563a668c93e7b4482
Mansi M. Shah <mmshah1990@gmail.com>
2017-01-24 18:56:14 +0530

    Updated Form

--- uploadForm.jsx
+++ uploadForm.jsx
@@ -1,20 +1,53 @@
 import React from 'react';
 
 class UploadForm extends React.Component {
-   render() {
+
+	constructor(props){
+		super(props);
+    }	
+
+	handleEmailToChange(e) {
+	   this.setState({email_to: e.target.value});
+	   console.log('Email To'+e.target.value);
+	}
+
+	handleEmailFromChange(e) {
+	   this.setState({email_from: e.target.value});
+	   console.log('Email From'+e.target.value);
+	}
+
+	handleMessageChange(e) {
+	   this.setState({message: e.target.value});
+	   console.log('message'+e.target.value);
+	}
+
+	handleAttachmentChange(e) {
+	   this.setState({attachment: e.target.value});
+	   console.log('attachment'+e.target.value);
+	}
+
+    render() {
 		return (
 			<div>
 				<h1>Upload File to Share</h1>
-				<form>
-					<input type="email" name="email_to" /> <br/>
-					<input type="email" name="email_from" /> <br/>
-					<textarea name="message" rows="10" cols="30" /> <br/>
-					<input type="file" name="attachment" accept="image/*" /> <br/>
+
+          		<form id="form1" action={this.props.action} method={this.props.method} onSubmit={this.onSubmit}>
+					<input type="email" name="email_to" onChange={this.handleEmailToChange} /> <br/>
+					<input type="email" name="email_from" onChange={this.handleEmailFromChange} /> <br/>
+					<textarea name="message" rows="10" cols="30" onChange={this.handleMessageChange}  /> <br/>
+					<input type="file" name="attachment" accept="image/*" onChange={this.handleAttachmentChange} /> <br/>
 					<input type="submit" />
 				</form>
 			</div>
 		);
-   }
+   	}
 }
+
+UploadForm.defaultProps = {
+		action: 'http:/api/v1/transfers/upload_file',
+		method: 'post',
+};
+
+module.exports = UploadForm;
 
 export default UploadForm;
