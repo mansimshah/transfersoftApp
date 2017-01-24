1360a5fc600b0fc8a991f253fedf45d21673d0af
Mansi M. Shah <mmshah1990@gmail.com>
2017-01-24 12:37:50 +0530

    Configured app environment

---
+++ main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import uploadFile from './uploadFile.jsx';
+
+ReactDOM.render(<uploadFile />, document.getElementById('app'));
---
+++ uploadFile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class uploadFile extends React.Component {
+   render() {
+		return (
+		 <div></div>
+		);
+   }
+}
+
+export default uploadFile;
